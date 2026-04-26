941978a80bf99649a193175e5b088bad7fbd9a8d
bn1654 <stikenloni@gmail.com>
2026-04-26 18:50:39 +0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'bn1654/collect',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'bn1654/auth',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8c5929af3ed7c6e506de5c03da8548f7811a1c81',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'bn1654/collect' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'bn1654/auth' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8c5929af3ed7c6e506de5c03da8548f7811a1c81',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
